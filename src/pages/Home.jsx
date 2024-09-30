472613cb99b86ca6a51c78d176dc5688b34fab93
Nikhil Pellakuru <76831568+nikrp@users.noreply.github.com>
2024-09-29 21:06:44 -0700

    Turn off

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -4,6 +4,66 @@ import { FaMoneyBills } from "react-icons/fa6";
 import summary_page_snap from '../assets/summary_page_snap.png';
 import total_equity_snap from '../assets/total_equity_snap.png';
 import net_equity_snap from '../assets/net_equity_snap.png';
+import { LineChart, Line, XAxis, YAxis, CartesianGrid, Tooltip, Legend, ResponsiveContainer, Area, AreaChart } from 'recharts';
+
+const data = [
+    {
+      name: 'Page A',
+      uv: 4000,
+      pv: 2400,
+      amt: 2400,
+    },
+    {
+      name: 'Page B',
+      uv: 3000,
+      pv: 1398,
+      amt: 2210,
+    },
+    {
+      name: 'Page C',
+      uv: 2000,
+      pv: 9800,
+      amt: 2290,
+    },
+    {
+      name: 'Page D',
+      uv: 2780,
+      pv: 3908,
+      amt: 2000,
+    },
+    {
+      name: 'Page E',
+      uv: 1890,
+      pv: 4800,
+      amt: 2181,
+    },
+    {
+      name: 'Page F',
+      uv: 2390,
+      pv: 3800,
+      amt: 2500,
+    },
+    {
+      name: 'Page G',
+      uv: 3490,
+      pv: 4300,
+      amt: 2100,
+    },
+  ];
+
+const data2 = [
+    { name: "Sep 5", price: 285 },
+    { name: "Sep 6", price: 282 },
+    { name: "Sep 7", price: 280 },
+    { name: "Sep 8", price: 279 },
+    { name: "Sep 11", price: 280 },
+    { name: "Sep 12", price: 278 },
+    { name: "Sep 13", price: 275 },
+    { name: "Sep 14", price: 270 },
+    { name: "Sep 15", price: 268 },
+    { name: "Sep 18", price: 265 },
+    { name: "Sep 19", price: 267 }
+]
 
 export default function Home() {
     return (
@@ -30,8 +90,8 @@ export default function Home() {
                     </p>
                 </div>
                 <div className={`w-1/2 relative flex justify-center`}>
-                    <div className={`w-8/12 aspect-video rounded-full opacity-15 bg-gradient-to-b from-emerald-950 to-blue-400 blur-xl z-10`}></div>
-                    <div className={`w-2/5 aspect-square absolute right-2 top-5 opacity-75 rounded-full bg-gradient-to-tr from-emerald-950 to-blue-400 p-16 blur-lg z-20`}>
+                    <div className={`w-8/12 aspect-video rounded-full opacity-60 bg-gradient-to-b from-emerald-950 to-blue-400 blur-xl z-10`}></div>
+                    <div className={`w-2/5 aspect-square absolute right-2 top-5 rounded-full bg-gradient-to-tr from-emerald-950 to-blue-400 p-20 blur-xl z-20`}>
                         <div className={`bg-gray-950 rounded-full w-full h-full`}></div>
                     </div>
 
@@ -40,9 +100,29 @@ export default function Home() {
                         <p className={`text-2xl flex items-center justify-between font-semibold`}><span>$100,038.60</span><span className={`text-green-400 text-sm font-medium`}> +0.039% <span className={`text-gray-400`}> +$38.60</span></span></p>
                     </div>
 
-                    <div className={`bg-base-200 z-40 opacity-60 h-full w-3/6 absolute left-20 top-5 shadow-lg border border-neutral text-white p-3 rounded-lg flex flex-col justify-between`}>
-                        <p className={`font-semibold text-gray-400 flex mb-2 justify-between`}><span>Total Equity</span><span className={`bg-green-500 bg-opacity-50 border-2 rounded-md border-green-500 border-opacity-75`}><FaMoneyBills color="white" className={`m-1.5`} size={25} /></span></p>
-                        <p className={`text-2xl flex items-center justify-between font-semibold`}><span>$100,038.60</span><span className={`text-green-400 text-sm font-medium`}> +0.039% <span className={`text-gray-400`}> +$38.60</span></span></p>
+                    <div className={`bg-base-200 z-40 opacity-60 h-full w-4/5 absolute left-10 top-5 shadow-lg border border-neutral text-white p-3 rounded-lg flex flex-col justify-between`}>
+                        <p className={`font-semibold text-gray-400 text-2xl flex mb-2`}><span>Cash</span></p>
+                        <ResponsiveContainer width="100%" height="100%">
+                            <AreaChart width={200} height={66} data={data}>
+                                <Area type="monotone" dataKey="pv" stroke="#007BFF" fill="#007BFF" strokeWidth={1} />
+                            </AreaChart>
+                        </ResponsiveContainer>
+                    </div>
+
+                    <div className={`bg-base-200 z-50 opacity-60 aspect-video w-1/4 absolute -bottom-10 right-10 shadow-lg border border-neutral text-white p-3 rounded-lg flex flex-col justify-between`}>
+                        <div className={`flex items-center gap-2 mb-1`}>
+                            <img src={`https://logo.clearbit.com/tesla.com`} className={`w-10 aspect-square rounded-full`} />
+                            <div>
+                                <p className={`font-semibold`}>TSLA</p>
+                                <p>Tesla Inc.</p>
+                            </div>
+                        </div>
+                        <ResponsiveContainer width="100%" height="100%">
+                            <AreaChart width={200} height={44} data={data2}>
+                                <YAxis hide domain={[264, 285]} />
+                                <Area type="monotone" dataKey="price" stroke="#FF6347" fill="#FF6347" strokeWidth={1} />
+                            </AreaChart>
+                        </ResponsiveContainer>
                     </div>
                 </div>
             </div>
